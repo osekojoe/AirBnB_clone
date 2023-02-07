09a6db6a51be80434f4cc058fbf6dd76a9a40c52
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-07 09:01:38 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,39 @@
 
 
 import uuid
-import datetime
+from datetime import datetime
+
+
+format_time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
     """defines all common attributes/methods for other classes"""
-    id = str(uuid.uuid4())
-    created_at = datetime.datetime.now()
-    updated_at = datetime.datetime.now()
+
+    def __init__(self, *args, **kwargs):
+        """re-create an instance with this dictionary representation
+        <class 'BaseModel'> -> to_dict() -> <class 'dict'> ->
+        <class 'BaseModel'> """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__Class__":
+                    setattr(self, key, value)
+            
+            if kwargs.get("created_at") and type(self.created_at) is str:
+                    self.created_at = datetime.strptime(kwargs["created_at"], format_time)
+            else:
+                self.created_at = datetime.utcnow()
+
+            if kwargs.get("updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], format_time)
+            else:
+                self.updated_at = datetime.utcnow()
+
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """string representation of BaseModel class"""
@@ -19,12 +45,13 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at with the
         current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ 
         of the instance"""
         new_dict = self.__dict__.copy()
+
         new_dict["__class__"] = self.__class__.__name__
 
         if "created_at" in new_dict:
