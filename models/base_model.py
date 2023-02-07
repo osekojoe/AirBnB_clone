d8b4632440079d9434c48ea2aabb84f5b2fdbfc7
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-07 03:51:31 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -13,5 +13,24 @@ class BaseModel:
 
     def __str__(self):
         """string representation of BaseModel class"""
-        return ("[{:s}] (:s) {}".format(self.__class__.name__,self.id,
+        return ("[{:s}] ({:s}) {}".format(self.__class__.__name__,self.id,
                                         self.__dict__))
+
+    def save(self):
+        """updates the public instance attribute updated_at with the
+        current datetime"""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ 
+        of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].isoformat()
+
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+
+        return new_dict
