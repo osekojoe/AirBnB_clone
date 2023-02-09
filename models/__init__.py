20ab88d3bb51e4a129d9462b9f5e91f8b2a2f07b
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-09 02:14:06 -0800

    create unique filestorage instance

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,10 @@
 The __init__.py file is used to mark a directory as a Python package. 
 It's executed when the package is imported, and it  contains initialization code for the package.
 """
+
+
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
