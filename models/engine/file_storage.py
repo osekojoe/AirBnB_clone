52940ce6cb36a464f81a882b94af26631ec74e8f
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-09 02:15:14 -0800

    serializes instances to a JSON file and deserializes JSON file to instances

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,7 +21,7 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
-            key = obj.__class__.name__ + "." + obj.id
+            key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
