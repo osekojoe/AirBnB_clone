84758ce5591f5a10062e50897af36363db2e9fe2
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-09 03:46:57 -0800

    Console 0.0.1

--- console.py
+++ console.py
@@ -13,9 +13,21 @@ class HBNBCommand(cmd.Cmd):
         """exit program"""
         return True
 
+    def help_quit(self):
+        """quit - help documentation"""
+        print("Exits the program with formatting\n")
+
     def do_EOF(self, arg):
         """exit program"""
         return True
+
+    def help_EOF(self):
+        """EOF - help documentation"""
+        print("Exits the program without formatting\n")
+
+    def emptyline(self):
+        """an empty line + ENTER shouldn’t execute anything"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
