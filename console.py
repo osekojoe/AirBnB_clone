74685c281e796b4ca3ece6cff2633f64a09f07d9
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-09 04:17:25 -0800

    Console 0.1

--- console.py
+++ console.py
@@ -14,7 +14,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def help_quit(self):
-        """quit - help documentation"""
+        """help for exit method"""
         print("Exits the program with formatting\n")
 
     def do_EOF(self, arg):
@@ -22,12 +22,44 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def help_EOF(self):
-        """EOF - help documentation"""
+        """help for EOF method"""
         print("Exits the program without formatting\n")
 
     def emptyline(self):
         """an empty line + ENTER shouldn’t execute anything"""
         pass
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id. Ex: $ create BaseModel"""
+        try:
+            if not args:
+                raise SyntaxError()
+            my_args = args.split(' ')
+            if my_args[0] not in HBNBCommand.classes:
+                raise NameError()
+
+            obj = eval("{}()".format(my_args[0]))
+            for arg in my_args[1:]:
+                if "=" not in arg:
+                    continue
+                attrs = arg.split("=")
+                key = attrs[0]
+                value = attrs[1]
+                value = value.replace('_', ' ')
+                setattr(obj, key, eval(value))
+            obj.save()
+
+            print("{}".format(obj.id))
+        except NameError:
+            print("** class doesn't exist **")
+        except SyntaxError:
+            print("** class name missing **")
+
+    def help_create(self):
+        """help for create method"""
+        print("Creates a new instance of class")
+        print("[Usage]: create <className>\n")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
