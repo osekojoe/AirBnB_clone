780d4b21f8beddbfc2ae5fe805506ab3a787fc80
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-10 08:19:17 -0800

    user

--- models/user.py
+++ models/user.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 """class User that inherits from BaseModel"""
+
+
+from base_model import BaseModel
+
+
+class User(BaseModel):
+    """User"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
