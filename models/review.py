fd3bf53901dd23bc81ffb496ae98c0004bffe351
COLLINS  TONUI <92209537+tckiprotich@users.noreply.github.com>
2023-02-10 18:39:14 +0000

    public class Review

--- models/review.py
+++ models/review.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 """Review"""
 
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """public class Review"""
+    place_id = ""
+    user_id = ""
+    text = ""
+
