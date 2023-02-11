4bad42e61d907a6b270d3946df44ee37d23e88a4
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-11 04:36:26 -0800

    more classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,11 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import
+from models.place import Place
+from models.review import Review
 
 
 classes = {'BaseModel': BaseModel, 'User': User, 'State': State, 'City': City,
