47e015f1ed3ababb4cb9742e88e1c2473e754c59
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-12 11:06:14 -0800

    pycodestyle fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """
-The __init__.py file is used to mark a directory as a Python package. 
-It's executed when the package is imported, and it  contains initialization code for the package.
+The __init__.py file is used to mark a directory as a Python package.
+It's executed when the package is imported, and it  contains initialization
+code for the package.
 """
 
 
